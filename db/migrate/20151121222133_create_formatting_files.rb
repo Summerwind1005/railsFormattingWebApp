5aabbfa008180b852d3adbaf80cd0b263f13ad15
Summerwind1005 <tamara.vietrova@gmail.com>
2015-11-30 01:07:12 +0100

    ignore schema

--- db/migrate/20151121222133_create_formatting_files.rb
+++ db/migrate/20151121222133_create_formatting_files.rb
@@ -2,7 +2,7 @@ class CreateFormattingFiles < ActiveRecord::Migration
   def change
     create_table :formatting_files do |t|
       t.string :name
-      t.string :extention
+      t.string :extension
       t.timestamps
     end
   end
